dee71190424f4bb7bca9776c05b4a30ae459eee3
howudong <tjdvy963@naver.com>
2023-11-10 16:17:26 +0900

    feat : MenuType 각 enum에 해당하는 Products 리스트 추가

--- src/main/java/christmas/domain/MenuType.java
+++ src/main/java/christmas/domain/MenuType.java
@@ -1,5 +1,18 @@
 package christmas.domain;
 
+import java.util.List;
+
+import static christmas.domain.Products.*;
+
 public enum MenuType {
-    DESERT, MAIN, BEVERAGE, APPETIZER;
+    APPETIZER(List.of(MUSHROOM_SOUP, TAPAS, CAESAR_SALAD)),
+    MAIN(List.of(T_BONE_STAKE, BBQ_LIBS, SEA_FOOD_PASTA, CHRISTMAS_PASTA)),
+    BEVERAGE(List.of(ZERO_COKE, RED_WINE, CHAMPAGNE)),
+    DESERT(List.of(ICECREAM, CHOCOLATE_CAKE));
+
+    private final List<Products> products;
+
+    MenuType(List<Products> products) {
+        this.products = products;
+    }
 }
