63f88ad1f88bf7b24b05e5a4265361df62dbd42d
howudong <tjdvy963@naver.com>
2023-11-10 19:02:58 +0900

    feat : 개수 초과 검사와 DRINK만 주문했는지를 검사하는 로직 추가

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -1,24 +1,41 @@
 package christmas.domain;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static christmas.util.ErrorHandler.INVALID_ORDER;
 import static christmas.util.ErrorHandler.getText;
 
 public final class Orders {
+    private static final int MAX_ORDER_SIZE = 20;
     private final List<Order> orders;
 
     public Orders(List<Order> orders) {
+        validate(orders);
+        this.orders = orders;
+    }
+
+    private void validate(List<Order> orders) {
+        validateEmpty(orders);
         orders.forEach(this::validateExistProduct);
         validateSameName(orders);
-        validateEmpty(orders);
-        this.orders = orders;
+        validateOrderSize(orders);
+        validateOnlyDrink(orders);
     }
 
     private void validateEmpty(List<Order> orders) {
         if (orders.isEmpty()) {
+            throw new IllegalArgumentException(getText(INVALID_ORDER));
+        }
+    }
+
+    private void validateExistProduct(Order order) {
+        boolean isMatch = Arrays.stream(Product.values())
+                .anyMatch(e -> e.getName().equals(order.name()));
+
+        if (!isMatch) {
             throw new IllegalArgumentException(getText(INVALID_ORDER));
         }
     }
@@ -34,12 +51,26 @@ public final class Orders {
         }
     }
 
-    private void validateExistProduct(Order order) {
-        boolean isMatch = Arrays.stream(Product.values())
-                .anyMatch(e -> e.getName().equals(order.name()));
-
-        if (!isMatch) {
+    private void validateOrderSize(List<Order> orders) {
+        if (orders.size() > MAX_ORDER_SIZE) {
             throw new IllegalArgumentException(getText(INVALID_ORDER));
         }
+    }
+
+    private void validateOnlyDrink(List<Order> orders) {
+        Set<MenuType> orderMenuTypes = convertMenuTypes(orders);
+
+        boolean isOnlyDrink = orderMenuTypes.stream()
+                .allMatch(e -> e.equals(MenuType.DRINK));
+
+        if (isOnlyDrink) {
+            throw new IllegalArgumentException(getText(INVALID_ORDER));
+        }
+    }
+
+    private Set<MenuType> convertMenuTypes(List<Order> orders) {
+        return orders.stream()
+                .map(e -> MenuType.getMenuTypeByName(e.name()))
+                .collect(Collectors.toSet());
     }
 }
