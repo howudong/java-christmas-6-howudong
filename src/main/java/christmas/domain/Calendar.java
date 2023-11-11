b6803ca31acfd793781701d47e2e1238c869a677
howudong <tjdvy963@naver.com>
2023-11-11 14:29:52 +0900

    feat : 주말,평일 구분을  위해 첫번째 주말할인 적용 일자도 인자로 받게함

--- src/main/java/christmas/domain/Calendar.java
+++ src/main/java/christmas/domain/Calendar.java
@@ -3,13 +3,19 @@ package christmas.domain;
 import java.util.List;
 
 public enum Calendar {
-    DECEMBER(31, List.of(3, 10, 17, 24, 25, 31));
+    DECEMBER(List.of(1, 2), 31, List.of(3, 10, 17, 24, 25, 31));
+    private final List<Integer> firstWeekendDays;
     private final int lastDay;
     private final List<Integer> discountDay;
 
-    Calendar(int lastDay, List<Integer> discountDay) {
+    Calendar(List<Integer> firstWeekendDays, int lastDay, List<Integer> discountDay) {
+        this.firstWeekendDays = firstWeekendDays;
         this.lastDay = lastDay;
         this.discountDay = discountDay;
+    }
+
+    public List<Integer> getFirstWeekendDays() {
+        return firstWeekendDays;
     }
 
     public boolean isStarDay(int day) {
