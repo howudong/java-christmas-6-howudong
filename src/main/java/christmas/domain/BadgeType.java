92a0a459455e631be8606df5aff5b1c94c63a36f
howudong <tjdvy963@naver.com>
2023-11-11 20:23:11 +0900

    feat : 배지 종류에 관한 enum class 생성

---
+++ src/main/java/christmas/domain/BadgeType.java
@@ -0,0 +1,22 @@
+package christmas.domain;
+
+public enum BadgeType {
+    STAR("별", 5_000L),
+    TREE("트리", 10_000L),
+    SANTA("산타", 20_000L);
+    private final String badgeName;
+    private final Long goalPrice;
+
+    BadgeType(String badgeName, Long goalPrice) {
+        this.badgeName = badgeName;
+        this.goalPrice = goalPrice;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
+
+    public Long getGoalPrice() {
+        return goalPrice;
+    }
+}
