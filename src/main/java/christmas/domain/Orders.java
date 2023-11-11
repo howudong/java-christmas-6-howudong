fc2562f5634d626d294e604c18e9ba660a34e3bd
howudong <tjdvy963@naver.com>
2023-11-11 11:36:46 +0900

    feat : Orders의 예약 월,일 추가 및 날짜 검증에 대한 로직 추가

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -5,64 +5,76 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static christmas.util.ErrorHandler.INVALID_ORDER;
-import static christmas.util.ErrorHandler.getText;
+import static christmas.util.ErrorHandler.*;
 
 public final class Orders {
     private static final int MAX_ORDER_SIZE = 20;
-    private final List<Order> orders;
+    private static final int FIRST_DAY = 1;
+    private final List<OrderProduct> orderProducts;
+    private final Calendar orderMonth;
+    private final int orderDay;
 
-    public Orders(List<Order> orders) {
-        validate(orders);
-        this.orders = orders;
+    public Orders(List<OrderProduct> orderProducts, Calendar orderMonth, int orderDay) {
+        validateOrderProduct(orderProducts);
+        getValidateDate(orderMonth, orderDay);
+        this.orderProducts = orderProducts;
+        this.orderMonth = orderMonth;
+        this.orderDay = orderDay;
     }
 
-    private void validate(List<Order> orders) {
-        validateEmpty(orders);
-        orders.forEach(this::validateExistProduct);
-        validateSameName(orders);
-        validateEmptyQuantity(orders);
-        validateOrderSize(orders);
-        validateOnlyDrink(orders);
+    private void getValidateDate(Calendar orderMonth, int orderDay) {
+        int lastDay = orderMonth.getLastDay();
+        if (orderDay < FIRST_DAY || orderDay > lastDay) {
+            throw new IllegalArgumentException(getText(INVALID_DATE));
+        }
     }
 
-    private void validateEmpty(List<Order> orders) {
-        if (orders.isEmpty()) {
+    private void validateOrderProduct(List<OrderProduct> orderProducts) {
+        validateEmpty(orderProducts);
+        orderProducts.forEach(this::validateExistProduct);
+        validateSameName(orderProducts);
+        validateEmptyQuantity(orderProducts);
+        validateOrderSize(orderProducts);
+        validateOnlyDrink(orderProducts);
+    }
+
+    private void validateEmpty(List<OrderProduct> orderProducts) {
+        if (orderProducts.isEmpty()) {
             throw new IllegalArgumentException(getText(INVALID_ORDER));
         }
     }
 
-    private void validateExistProduct(Order order) {
+    private void validateExistProduct(OrderProduct orderProduct) {
         boolean isMatch = Arrays.stream(Product.values())
-                .anyMatch(e -> e.getName().equals(order.name()));
+                .anyMatch(e -> e.getName().equals(orderProduct.name()));
 
         if (!isMatch) {
             throw new IllegalArgumentException(getText(INVALID_ORDER));
         }
     }
 
-    private void validateSameName(List<Order> orders) {
-        int distinctSize = orders.stream()
-                .map(Order::name)
+    private void validateSameName(List<OrderProduct> orderProducts) {
+        int distinctSize = orderProducts.stream()
+                .map(OrderProduct::name)
                 .collect(Collectors.toSet())
                 .size();
 
-        if (orders.size() != distinctSize) {
+        if (orderProducts.size() != distinctSize) {
             throw new IllegalArgumentException(getText(INVALID_ORDER));
         }
     }
 
-    private void validateEmptyQuantity(List<Order> orders) {
-        boolean containEmpty = orders.stream().anyMatch(e -> e.quantity() == 0);
+    private void validateEmptyQuantity(List<OrderProduct> orderProducts) {
+        boolean containEmpty = orderProducts.stream().anyMatch(e -> e.quantity() == 0);
 
         if (containEmpty) {
             throw new IllegalArgumentException(getText(INVALID_ORDER));
         }
     }
 
-    private void validateOrderSize(List<Order> orders) {
-        int totalOrderSize = orders.stream()
-                .map(Order::quantity)
+    private void validateOrderSize(List<OrderProduct> orderProducts) {
+        int totalOrderSize = orderProducts.stream()
+                .map(OrderProduct::quantity)
                 .reduce(Integer::sum)
                 .orElse(0);
 
@@ -71,8 +83,8 @@ public final class Orders {
         }
     }
 
-    private void validateOnlyDrink(List<Order> orders) {
-        Set<MenuType> orderMenuTypes = convertMenuTypes(orders);
+    private void validateOnlyDrink(List<OrderProduct> orderProducts) {
+        Set<MenuType> orderMenuTypes = convertMenuTypes(orderProducts);
         boolean isOnlyDrink = orderMenuTypes.stream()
                 .allMatch(e -> e.equals(MenuType.DRINK));
 
@@ -81,8 +93,8 @@ public final class Orders {
         }
     }
 
-    private Set<MenuType> convertMenuTypes(List<Order> orders) {
-        return orders.stream()
+    private Set<MenuType> convertMenuTypes(List<OrderProduct> orderProducts) {
+        return orderProducts.stream()
                 .map(e -> MenuType.getMenuTypeByName(e.name()))
                 .collect(Collectors.toSet());
     }
