358904d6703655bb3e0770718dd4e4864a7bac22
howudong <tjdvy963@naver.com>
2023-11-12 17:05:22 +0900

    feat : InputDto로부터 입력을 totalPrice를 얻는 함수 추가

--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -1,7 +1,12 @@
 package christmas.service;
 
+import christmas.domain.Calendar;
+import christmas.domain.OrderProduct;
+import christmas.domain.Orders;
 import christmas.dto.OrderDto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public final class OrderService {
@@ -10,5 +15,24 @@ public final class OrderService {
         int orderDay = dto.day();
 
         return new OrderDto.Output(order, orderDay);
+    }
+
+    public Long getOriginalTotalPrice(OrderDto.Input dto) {
+        List<OrderProduct> orders = convert(dto.orderProducts());
+        int orderDay = dto.day();
+        return getOrders(orders, orderDay).getOriginalPrice();
+    }
+
+    private List<OrderProduct> convert(Map<String, Integer> orderProducts) {
+        List<OrderProduct> orders = new ArrayList<>();
+
+        orderProducts.forEach((key, value)
+                -> orders.add(new OrderProduct(key, value)));
+
+        return orders;
+    }
+
+    private Orders getOrders(List<OrderProduct> orders, int orderDay) {
+        return new Orders(orders, Calendar.DECEMBER, orderDay);
     }
 }
