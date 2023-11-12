742b295bb0d5599c914243a36cab13ddd508e2ca
howudong <tjdvy963@naver.com>
2023-11-12 22:08:08 +0900

    feat : InputValidator 클래스를 통해 숫자만 있는지, 한글 문자만 있는지, 특정 키워드가 특정 개수가 있는지를 검사

---
+++ src/main/java/christmas/view/inputview/InputValidator.java
@@ -0,0 +1,32 @@
+package christmas.view.inputview;
+
+import christmas.util.ErrorHandler;
+
+public final class InputValidator {
+
+    private static final String ONLY_KOREAN_WORD = "[^가-힣]";
+
+    public void isNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorHandler.getText(ErrorHandler.INVALID_DATE));
+        }
+    }
+
+    public void hasExactlyContains(char target, int count, String input) {
+        int containSize = (int) input.chars().filter(e -> e == target).count();
+        if (containSize != count) {
+            throw new IllegalArgumentException(ErrorHandler.getText(ErrorHandler.INVALID_ORDER));
+        }
+    }
+
+    public void isOnlyWords(String input) {
+        boolean hasContainNotKoreanWord = input.chars()
+                .anyMatch(e -> String.valueOf((char) e).matches(ONLY_KOREAN_WORD));
+        
+        if (hasContainNotKoreanWord) {
+            throw new IllegalArgumentException(ErrorHandler.getText(ErrorHandler.INVALID_ORDER));
+        }
+    }
+}
