16f70e7787b75d4e672a19f4a37d5be4db292611
howudong <tjdvy963@naver.com>
2023-11-13 15:50:08 +0900

    feat : DiscountDto에 할인 후 금액 추가

--- src/main/java/christmas/dto/DiscountDto.java
+++ src/main/java/christmas/dto/DiscountDto.java
@@ -2,12 +2,18 @@ package christmas.dto;
 
 public final class DiscountDto extends OutputDto {
     private final Long benefitPrice;
+    private final Long totalDiscountPrice;
 
-    public DiscountDto(Long benefitPrice) {
+    public DiscountDto(Long benefitPrice, Long totalDiscountPrice) {
         this.benefitPrice = benefitPrice;
+        this.totalDiscountPrice = totalDiscountPrice;
     }
 
     public Long benefitPrice() {
         return benefitPrice;
+    }
+
+    public Long getTotalDiscountPrice() {
+        return totalDiscountPrice;
     }
 }
