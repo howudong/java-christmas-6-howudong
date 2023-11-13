ab6d1e6a609ad624f60d814c3a3076929a7caa4a
howudong <tjdvy963@naver.com>
2023-11-13 23:18:47 +0900

    feat :DiscountDto를 생성하도록 DiscountService 정의

---
+++ src/main/java/christmas/service/DiscountService.java
@@ -0,0 +1,23 @@
+package christmas.service;
+
+import christmas.domain.discounts.DiscountCalculator;
+import christmas.dto.DiscountDto;
+
+import java.util.Map;
+
+public final class DiscountService {
+    private final DiscountCalculator calculator;
+
+    public DiscountService(DiscountCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public DiscountDto createDiscountDto() {
+        Map<String, Long> discounts = calculator.getAvailableDiscounts();
+        return new DiscountDto(discounts, sumAllPrice(discounts));
+    }
+
+    private Long sumAllPrice(Map<String, Long> discounts) {
+        return discounts.values().stream().reduce(Long::sum).orElse(0L);
+    }
+}
