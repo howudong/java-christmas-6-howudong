ccea8246809929e694c3ce6a7d1712f7431d7081
howudong <tjdvy963@naver.com>
2023-11-13 22:49:25 +0900

    feat : BenefitController 연결

--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -1,10 +1,13 @@
 package christmas.controller;
 
 import christmas.dto.InputDto;
+import christmas.dto.OrderDto;
 import christmas.dto.OutputDto;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static christmas.view.Parameter.Output.ORDER_DTO;
 
 public final class MainController {
     private final ControlConfig config;
@@ -16,12 +19,19 @@ public final class MainController {
     public void run() {
         Map<String, InputDto> inputs = new HashMap<>();
         Map<String, OutputDto> outputs = new HashMap<>();
-        
+
         controlOrder(inputs, outputs);
+        controlBenefit(inputs, outputs);
     }
 
     private void controlOrder(Map<String, InputDto> inputs, Map<String, OutputDto> outputs) {
         Controller orderController = config.createOrderController();
         orderController.process(inputs, outputs);
+    }
+
+    private void controlBenefit(Map<String, InputDto> inputs, Map<String, OutputDto> outputs) {
+        OrderDto.Output outputDto = (OrderDto.Output) outputs.get(ORDER_DTO);
+        Controller benefitController = config.createBenefitController(outputDto.toEntity());
+        benefitController.process(inputs, outputs);
     }
 }
