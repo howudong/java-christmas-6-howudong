560db8979dad491b0c2a880e069071ce8ada3e6a
howudong <tjdvy963@naver.com>
2023-11-13 17:33:16 +0900

    feat : MainController와 Application 연결 그리고, ControlConfig에 OrderController에 대한 구성 추가

--- src/main/java/christmas/controller/ControlConfig.java
+++ src/main/java/christmas/controller/ControlConfig.java
@@ -1,4 +1,12 @@
 package christmas.controller;
 
+import christmas.service.OrderService;
+import christmas.view.inputview.InputValidator;
+import christmas.view.inputview.OrderInputView;
+import christmas.view.outputview.OrderOutputView;
+
 public final class ControlConfig {
+    public Controller createOrderController() {
+        return new OrderController(new OrderService(), new OrderInputView(new InputValidator()), new OrderOutputView());
+    }
 }
--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -1,9 +1,27 @@
 package christmas.controller;
 
+import christmas.dto.InputDto;
+import christmas.dto.OutputDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public final class MainController {
     private final ControlConfig config;
 
     public MainController(ControlConfig config) {
         this.config = config;
+    }
+
+    public void run() {
+        Map<String, InputDto> inputs = new HashMap<>();
+        Map<String, OutputDto> outputs = new HashMap<>();
+        
+        controlOrder(inputs, outputs);
+    }
+
+    private void controlOrder(Map<String, InputDto> inputs, Map<String, OutputDto> outputs) {
+        Controller orderController = config.createOrderController();
+        orderController.process(inputs, outputs);
     }
 }
