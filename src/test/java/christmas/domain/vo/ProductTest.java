c6524b072558449bf9be34dda52cffaf8bc4eda4
howudong <tjdvy963@naver.com>
2023-11-14 20:10:07 +0900

    test : 일부 테스트들을 Product 도메인으로 옮김

---
+++ src/test/java/christmas/domain/vo/ProductTest.java
@@ -0,0 +1,27 @@
+package christmas.domain.vo;
+
+import christmas.domain.OrderProduct;
+import christmas.util.ErrorHandler;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static christmas.domain.vo.Product.CHOCOLATE_CAKE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ProductTest {
+    @Test
+    @DisplayName("Product에 없는 메뉴의 이름이 포함되어 있다면 예외를 발생시킨다.")
+    void 해당_메뉴_없음_예외() {
+        assertThatThrownBy(() -> new OrderProduct(null, 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorHandler.INVALID_ORDER);
+    }
+
+    @Test
+    @DisplayName("주문 개수가 0개인 것이 있다면 예외를 발생시킨다.")
+    void 주문_개수_0개_예외() {
+        assertThatThrownBy(() -> new OrderProduct(CHOCOLATE_CAKE, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorHandler.INVALID_ORDER);
+    }
+}
